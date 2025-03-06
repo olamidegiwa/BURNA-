b5130160741d2719c7f03a8df05e901cfcb88302
olamidegiwa <giwasodiq7@gmail.com>
2025-03-06 12:27:54 +0100

    updated

--- src/components/Preload.jsx
+++ src/components/Preload.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Preload = () => {
   return (
-    <div className="fixed w-full h-[100vh] flex justify-center place-items-center">
+    <div className="fixed w-full h-[100vh] flex justify-center place-items-center gap-3">
       <h1 className="text-7xl custom-text-gold ">BIG 7</h1>
       <p className="text-7xl">🦍</p>
     </div>
